1fd0275b2435a9ec7f93981fe672827349dd6271
Tony Thornton <tonytmtsh@gmail.com>
2023-01-15 20:14:47 -0500

    added elements

---
+++ lib/element.dart
@@ -0,0 +1,13 @@
+class Element {
+  final int? id;
+  final String? name;
+  final String? thumbnail;
+  final String? data;
+
+  Element({
+    this.id,
+    this.name,
+    this.thumbnail,
+    this.data,
+  });
+}
