99217efb7a5566ae999439eb2baaf640661662c9
TonyT <tonytmtsh@gmail.com>
2023-02-13 23:49:41 -0500

    push to web site again

--- lib/elements.dart
+++ lib/elements.dart
@@ -142,7 +142,7 @@ class ElementsType with ChangeNotifier {
     String xmlData = "<favourites>\n";
     for (Element e in elements) {
       xmlData +=
-          "  <favourite name=\"${htmlEscape.convert(e.name)}\" thumb=\"${htmlEscape.convert(e.thumb)}\">${htmlEscape.convert(e.data)}</favourite>\n";
+          "    <favourite name=\"${htmlEscape.convert(e.name)}\" thumb=\"${htmlEscape.convert(e.thumb)}\">${htmlEscape.convert(e.data)}</favourite>\n";
     }
     xmlData += "</favourites>\n";
 
